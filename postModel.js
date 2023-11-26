d48a48a3a931051082804fab72551ec046dce0f4
Sree1710 <sreelekshmisl1710@gmail.com>
2023-11-26 18:45:20 +0530

    Modifications Done

--- postModel.js
+++ postModel.js
@@ -1,6 +1,7 @@
 const mongoose=require('mongoose')
 const postModel=mongoose.model("posts",mongoose.Schema(
     {
+        post_id:String,
         postTitle:{type:String,required:true},
         postDesc:String,
         postImg:String
